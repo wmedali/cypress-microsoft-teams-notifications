bd3d8fa993da607ee60acd4786c4a755cc5c7052
Alee <monarc1937@gmail.com>
2022-04-15 15:23:22 +0200

    Start deploying

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-//
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license.
-//
-const crypto = require("crypto");
 const express = require("express");
 const axios = require("axios");
 const bodyParser = require("body-parser");
@@ -14,7 +9,10 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.static("reports"));
 
 var PORT = process.env.port || process.env.PORT || 8080;
-//Token = HXISR/4qUASmscgnrz0fuRKL7I9pLFye0XfihqSIlFQ=
+const token = "Azure devops token";
+
+// Change this azure devops url
+const base_url = "https://dev.azure.com/ORGANIZATION_NAME/PROJECT_NAME";
 
 var data = JSON.stringify({
   templateParameters: {},
@@ -23,16 +21,16 @@ var data = JSON.stringify({
 let getConfig = (buildId) => {
   return {
     method: "post",
-    url: `https://dev.azure.com/edenred-hq/FleetAndMobility/_apis/pipelines/${buildId}/runs?api-version=6.0-preview.1`,
+    url: `${base_url}/_apis/pipelines/${buildId}/runs?api-version=6.0-preview.1`,
     headers: {
-      Authorization: "Basic TOKEN",
+      Authorization: "Basic " + token,
       "Content-Type": "application/json",
     },
     data: data,
   };
 };
 
-let returnMessage = (runUrl, status = "Suivre l'execution") => {
+let returnMessage = (runUrl, status) => {
   return {
     type: "message",
     attachments: [
@@ -45,7 +43,7 @@ let returnMessage = (runUrl, status = "Suivre l'execution") => {
           body: [
             {
               type: "TextBlock",
-              text: "FuelMal commence à tourner, tu peux chercher ta bière en attendant ☕",
+              text: "Les tests tournent, tu peux chercher ta bière en attendant ☕",
             },
           ],
           actions: [
@@ -63,52 +61,44 @@ let returnMessage = (runUrl, status = "Suivre l'execution") => {
   };
 };
 
+// Supposing we have 2 pipelines to lunch, each pipeline has un unique id
 let getPipelineId = (scopeName) => {
   const pipelines = [
     {
-      name: "Extranet",
-      id: 384,
+      name: "SCOPE_1",
+      id: 108,
     },
     {
-      name: "Subscription",
-      id: 486,
-    },
-    {
-      name: "Authentication",
-      id: 418,
-    },
-    {
-      name: "Tests",
-      id: 438,
+      name: "SCOPA_2",
+      id: 103,
     },
   ];
 
   const pipeline = pipelines.find((pipeline) => pipeline.name === scopeName);
   return pipeline.id;
 };
-app.post("/fuelman", (req, res) => {
-  var payload = req.body;
+app.post("/mrbox", (req, res) => {
   try {
-    // Retrieve authorization HMAC information
-    var receivedMsg = payload;
-
     // The text received to webhook
-    var receivedText = receivedMsg.text;
+    var receivedText = req.body.text;
 
     // The message sent by webhook
     var responseMsg = "";
-    const apps = ["Extranet", "Subscription", "Authentication", "Tests"];
+    const apps = ["SCOPE_1", "SCOPE_2"];
     let app = apps.find((element) => receivedText.includes(element));
-    if (!app) app = "Tests";
+    if (!app) app = "SANITY";
 
     let buildId = getPipelineId(app);
-
+    // Add buildId Here
     let config = getConfig(buildId);
     // Creating adaptive card response
     axios(config)
-      .then(function (response) {
+      .then((response) => {
         console.log(JSON.stringify(response.data));
-        responseMsg = returnMessage(response.data._links.web.href);
+        responseMsg = returnMessage(
+          response.data._links.web.href,
+          "Suivre l'execution"
+        );
         res.json(responseMsg);
         res.end();
       })
